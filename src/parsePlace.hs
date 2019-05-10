c653bf85ae6573485a5146e40ea99666922583d1
Tom <tomwestbury1@gmail.com>
2019-05-10 15:07:07 +0100

    added parsePlace case for Dot
    reversed the input of the placenotTok in the input to allow for use of :

--- src/parsePlace.hs
+++ src/parsePlace.hs
@@ -42,10 +42,14 @@ lexPlace p
 
 toPn :: PlaceNotTok -> PlaceNotation
 toPn Xtok = X
-toPn Place p = Change [p]
+toPn Dot = Change []
+
+retParsePlace :: [PlaceNotTok] -> ([PlaceNotation],[PlaceNotTok])
+retParsePlace p = ([], (reverse p))
 
 parsePlace :: ([PlaceNotation], [PlaceNotTok]) -> [PlaceNotation]
-parsePlace (pns, []) = reverse pns
+parsePlace (pns, []) = pns
+parsePlace (((Change []) : pns), ((Place x) : ps) = (((Change [x]) : pns),ps)
 parsePlace (((Change cs):pns), (Place c):ps) = parsePlace (((Change (c:cs)):pns), ps)
 parsePlace ((pn:pns), (Palindrome : ps)) = (((reverse pns) ++ pn ++ pns), ps)
 parsePlace (pns, (p:ps)) = parsePlace (((toPN p):pns), ps)
